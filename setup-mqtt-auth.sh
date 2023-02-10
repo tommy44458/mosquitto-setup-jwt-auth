1cca63b0d48df89b58b13627b2566b6f3f3e1fce
Tommy Huang <free44458@gmail.com>
2023-02-10 16:53:26 +0800

    fix: no update go-auth.so

--- setup-mqtt-auth.sh
+++ setup-mqtt-auth.sh
@@ -2,24 +2,23 @@
 
 ROOT=$(dirname $0)
 
-if [ ! -f "/etc/mosquitto/conf.d/go-auth.so" ]; then
-
-    if [ ! -d "$ROOT/mosquitto-go-auth" ]; then
-        git clone https://github.com/tommy44458/mosquitto-go-auth.git $ROOT/mosquitto-go-auth
-    fi
-
-    if [ ! -f "$ROOT/mosquitto-go-auth/go-auth.so" ]; then
-        cd $ROOT/mosquitto-go-auth
-        make
-        cd -
-    fi
-
-    if [ ! -d "/etc/mosquitto/conf.d/" ]; then
-        sudo mkdir /etc/mosquitto/conf.d/
-    fi
-
-    sudo cp $ROOT/mosquitto-go-auth/go-auth.so /etc/mosquitto/conf.d/
+if [ -f "/etc/mosquitto/conf.d/go-auth.so" ]; then
+    sudo rm -rf /etc/mosquitto/conf.d/go-auth.so
 fi
+
+if [ ! -d "$ROOT/mosquitto-go-auth" ]; then
+    git clone https://github.com/tommy44458/mosquitto-go-auth.git $ROOT/mosquitto-go-auth
+fi
+
+cd $ROOT/mosquitto-go-auth
+make
+cd -
+
+if [ ! -d "/etc/mosquitto/conf.d/" ]; then
+    sudo mkdir /etc/mosquitto/conf.d/
+fi
+
+sudo cp $ROOT/mosquitto-go-auth/go-auth.so /etc/mosquitto/conf.d/
 
 sudo cp $ROOT/mqtt_broker/go-auth.conf /etc/mosquitto/conf.d/
 
